b0412636b7247509ff9b56c40f89f99f7b331042
Manav <manav.guptamg14@gmail.com>
2022-09-10 11:56:06 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils" aboutText="About Us" />
+    </>
+  );
+}
+
+export default App;
