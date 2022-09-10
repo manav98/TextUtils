99a59bc4644e2780787ff992863881de8ef81fe6
Manav <manav.guptamg14@gmail.com>
2022-09-10 12:33:51 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
-import './App.css';
-import Navbar from './components/Navbar';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About Us" />
+      <div className="container my-3">
+        <TextForm heading="Enter Text to analyze below" />
+      </div>
     </>
   );
 }
